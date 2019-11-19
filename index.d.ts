98a000a308c70a9a000ec9e11d9893ea6aa6f498
Sayantan Ghosh <sayantan.ghosh@impactanalytics.co>
2019-11-19 13:33:10 +0530

    1. Minor change in index.d.ts. (Added children prop inside each prop type)

--- index.d.ts
+++ index.d.ts
@@ -2,9 +2,11 @@ import React from 'react';
 
 type FallbackUIType = {
     fallbackUI: (props: any) => JSX.Element;
+    children: JSX.Element;
 }
 type ThemeType = {
     theme: string;
+    children: JSX.Element;
 }
 
 type CEBPropType = FallbackUIType | ThemeType;
